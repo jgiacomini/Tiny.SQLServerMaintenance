452bdbc83b09e90c16308797efdc4213c46501b2
Jérôme GIACOMINI <archeone@gmail.com>
2019-11-15 22:26:51 +0100

    Add wpf project

---
+++ src/Tiny.SQLServerMaintenanceApp/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace Tiny.SQLServerMaintenanceApp
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private bool _isBusy;
+        private string _connectionString;
+        public MainViewModel()
+        {
+            GetFragmentationCommand = new RelayCommand(GetFragmentation);
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return _connectionString;
+            }
+            set
+            {
+                Set(ref _connectionString, value);
+            }
+        }
+
+        private async void GetFragmentation()
+        {
+            var sqlServerMaintenanceClient = new SqlServerMaintenanceClient(ConnectionString);
+
+            IsBusy = true;
+            try
+            {
+                var framentation = await sqlServerMaintenanceClient.GetFragmentationAsync();
+            }
+            catch (System.Exception)
+            {
+            }
+
+            IsBusy = false;
+        }
+
+        public RelayCommand GetFragmentationCommand { get; }
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            set
+            {
+                Set(ref _isBusy, value);
+            }
+        }
+    }
+}
--- src/Tiny.SQLServerMaintenanceApp/SqlServerMaintenanceClient.cs
+++ src/Tiny.SQLServerMaintenanceApp/SqlServerMaintenanceClient.cs
@@ -50,7 +50,6 @@ namespace Tiny.SQLServerMaintenanceApp
 
                 return result;
             }
-
         }
     }
 
