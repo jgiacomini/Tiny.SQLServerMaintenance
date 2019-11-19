349d5304282f890237c698e8093814f7e469cb67
Jérôme GIACOMINI <archeone@gmail.com>
2019-11-19 23:56:28 +0100

    add vm base

---
+++ src/Tiny.SQLServerMaintenanceApp/Infra/AppViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Tiny.SQLServerMaintenanceApp
+{
+    public abstract class AppViewModelBase : ViewModelBase
+    {
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            set
+            {
+                Set(ref _isBusy, value);
+            }
+        }
+    }
+}
