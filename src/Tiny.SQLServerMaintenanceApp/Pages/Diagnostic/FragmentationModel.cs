dcc14364190632253f68da70c6904342fd081d50
Jérôme GIACOMINI <archeone@gmail.com>
2019-11-19 23:57:03 +0100

    add diagnostic

---
+++ src/Tiny.SQLServerMaintenanceApp/Pages/Diagnostic/FragmentationModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Tiny.SQLServerMaintenanceApp
+{
+    public class FragmentationModel : ObservableObject
+    {
+        public FragmentationModel(Fragmentation statistiques)
+        {
+            FragmentationInPercent = statistiques.FragmentationInPercent;
+            SchemaName = statistiques.SchemaName;
+            TableName = statistiques.TableName;
+            IndexName = statistiques.IndexName;
+        }
+
+        public double FragmentationInPercent { get; private set; }
+        public string SchemaName { get; private set; }
+        public string TableName { get; private set; }
+        public string IndexName { get; private set; }
+    }
+}
---
+++ src/Tiny.SQLServerMaintenanceApp/Pages/Diagnostic/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System.Collections.ObjectModel;
+
+namespace Tiny.SQLServerMaintenanceApp
+{
+    public class MainViewModel : AppViewModelBase
+    {
+        private string _connectionString;
+        public MainViewModel()
+        {
+            GetFragmentationCommand = new RelayCommand(GetFragmentation);
+            Fragmentations = new ObservableCollection<FragmentationModel>();
+            _connectionString = AppSettings.Default.ConnectionString;
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return _connectionString;
+            }
+            set
+            {
+                if (Set(ref _connectionString, value))
+                {
+                    AppSettings.Default.ConnectionString = value;
+                    AppSettings.Default.Save();
+                }
+            }
+        }
+
+        private async void GetFragmentation()
+        {
+            var sqlServerMaintenanceClient = new SqlServerMaintenanceClient(ConnectionString);
+
+            IsBusy = true;
+            try
+            {
+                var fragmentations = await sqlServerMaintenanceClient.GetFragmentationAsync();
+                Fragmentations.Clear();
+                foreach (var item in fragmentations)
+                {
+                    Fragmentations.Add(new FragmentationModel(item));
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+
+            IsBusy = false;
+        }
+
+        public ObservableCollection<FragmentationModel> Fragmentations { get; private set; }
+
+        public RelayCommand GetFragmentationCommand { get; }
+    }
+}
