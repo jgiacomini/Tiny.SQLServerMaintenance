b4a6d40630285984aa7cd90a952443a3b9c19f48
Jérôme GIACOMINI <archeone@gmail.com>
2020-02-17 15:26:30 +0100

    Split in 2 files

--- src/Tiny.SQLServerMaintenance/Fragmentation.cs
+++ src/Tiny.SQLServerMaintenance/Fragmentation.cs
@@ -37,16 +37,4 @@ namespace Tiny.SQLServerMaintenanceApp
             }
         }
     }
-
-    public class Index
-    {
-        public Index(string indexName, double fragmentationInPercent)
-        {
-            IndexName = indexName;
-            FragmentationInPercent = fragmentationInPercent;
-        }
-
-        public string IndexName { get; private set; }
-        public double FragmentationInPercent { get; private set; }
-    }
 }
