63d75fffc7ae29a10cbb9ed5f0572b5c23be9519
Jérôme GIACOMINI <archeone@gmail.com>
2020-02-19 10:43:00 +0100

    rename

--- src/Tiny.SQLServerMaintenance/Fragmentation.cs
+++ src/Tiny.SQLServerMaintenance/Fragmentation.cs
@@ -5,23 +5,23 @@ namespace Tiny.SQLServerMaintenanceApp
 {
     public class Fragmentation
     {
-        private List<Index> _indices;
+        private List<Index> _indexes;
 
         public Fragmentation(string schemaName, string tableName)
         {
             SchemaName = schemaName;
             TableName = tableName;
-            _indices = new List<Index>();
+            _indexes = new List<Index>();
         }
 
         public string SchemaName { get; set; }
         public string TableName { get; set; }
 
-        public IEnumerable<Index> Indexes { get { return _indices; } }
+        public IEnumerable<Index> Indexes { get { return _indexes; } }
 
         public void Add(Index index)
         {
-            _indices.Add(index);
+            _indexes.Add(index);
         }
 
         public double MaxFragmentation
